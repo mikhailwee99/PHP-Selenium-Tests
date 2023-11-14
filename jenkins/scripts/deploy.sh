3fbb3f2697722c47e06d40e1eea0f37864a51e9d
mikhailwee99 <44152470+mikhailwee99@users.noreply.github.com>
2023-11-15 00:07:35 +0800

    yes

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 3000:3000 --name my-apache-php-app -v /home/src:/var/www php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v /home/src:/var/www php:7.2-apache
 sleep 1
 set +x
 
 echo 'Now...'
-echo 'Visit http://localhost:3000 to see your PHP application in action.'
+echo 'Visit http://localhost to see your PHP application in action.'
