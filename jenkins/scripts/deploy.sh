c0b3e01d2de658bbd1154ee9d57168a89a423299
mikhailwee99 <44152470+mikhailwee99@users.noreply.github.com>
2023-11-14 23:48:44 +0800

    yes

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v C:\\Users\\mikha\\Desktop\\PHP\\PHP-Selenium-Tests\\src:/var/www php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v /home/src:/var/www php:7.2-apache
 sleep 1
 set +x
 
 echo 'Now...'
 echo 'Visit http://192.168.1.19 to see your PHP application in action.'
-
